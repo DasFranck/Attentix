f3e8f05478f396cfd1d502ab35de4464088c879a
DasFranck Hochstaetter <dasfranck@gmx.com>
2019-03-30 18:30:44 +0100

    Divide WaitingTimeGetter.py into itself and ContextGetter.py (classes/)

---
+++ classes/ContextGetter.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import datetime
+import os
+import requests
+
+import pyowm
+import holidays
+
+
+class ContextGetter():
+    HEADERS = { "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 6.0.1; Redmi 3S MIUI/8.9.6)" }
+    FRENCH_VACATION_URL = "https://www.data.gouv.fr/fr/datasets/r/000ae493-9fa8-4088-9f53-76d375204036"
+
+    owm = None
+    weather = None
+
+    def __init__(self):
+        self._init_owm()
+
+    def _init_owm(self):
+        self.owm = pyowm.OWM(API_key=os.getenv("OWM_API_KEY"), version="2.5")
+        self.weather = self.owm.weather_at_coords(49.135143, 2.565823).get_weather()
+
+    def get_holidays(self, date=datetime.date.today()):
+        """
+        Return a dict with every Holidays at the date passed in parameters.
+        If no parameter is passed, the function default to today's date.
+
+        Expected argument type: datetime.date
+        """
+        holidays_today = {
+            "French Holiday A Zone": False,
+            "French Holiday B Zone": False,
+            "French Holiday C Zone": False
+        }
+
+        holidays_today["French Public Holiday"] = True if date in holidays.France() else False
+
+        # Get french public holidays per zone
+        holidays_dict = requests.get(self.FRENCH_VACATION_URL, headers=self.HEADERS).json()
+        holidays_dict = [holidays_item["fields"] for holidays_item in holidays_dict if "Zone" in holidays_item["fields"]["zones"]]
+        for holidays_period in holidays_dict:
+            if datetime.date.fromisoformat(holidays_period["start_date"]) <= date < datetime.date.fromisoformat(holidays_period["end_date"]):
+                for zone in ("A", "B", "C"):
+                    if zone in holidays_period["zones"]:
+                        holidays_today[f"French Holiday {zone} Zone"] = True
+
+        return holidays_today
+
+    def get_context(self):
+        return {
+            "Holidays": self.get_holidays(),
+            "Weather": self.weather.get_detailed_status(),
+            "Temperature": self.weather.get_temperature("celsius")["temp"]
+        }
+
+if __name__ == "__main__":
+    from pprint import pprint
+    cg = ContextGetter()
+    pprint(cg.get_context())
--- classes/WaitingTimeGetter.py
+++ classes/WaitingTimeGetter.py
@@ -1,64 +1,29 @@
 #!/usr/bin/env python3
 
-import csv
 import locale
+import datetime
 import json
 import logging
-import os
 import requests
 
-import holidays
-import pyowm
-
 from collections import OrderedDict
-from datetime import datetime, date
 
 
 class WaitingTimeGetter:
     ATTRACTION_URL = "https://www.parcasterix.fr/webservices/api/attractions.json?device=android&version=320&apiversion=1&lang=fr"
     ATTENTIX_URL = "https://www.parcasterix.fr/webservices/api/attentix.json?device=android&version=320&apiversion=1&lang=fr"
-    FRENCH_VACATION_URL = "https://www.data.gouv.fr/fr/datasets/r/000ae493-9fa8-4088-9f53-76d375204036"
     HEADERS = { "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 6.0.1; Redmi 3S MIUI/8.9.6)" }
 
-    owm = None
-    weather = None
     attractions = {}
 
     def __init__(self):
-        self._init_owm()
         self._get_attractions()
         self._check_attractions()
-
-    def _init_owm(self):
-        self.owm = pyowm.OWM(API_key=os.getenv("OWM_API_KEY"), version="2.5")
-        self.weather = self.owm.weather_at_coords(49.135143, 2.565823).get_weather()
 
     def _get_attractions(self):
         with open("attractions.json") as attractions_file:
             attractions = json.load(attractions_file)
             self.attractions = OrderedDict(sorted(attractions.items(), key=lambda t: t[0]))
-
-    def _get_today_holidays(self):
-        today_date = date.today()
-
-        holidays_today = {
-            "French Holiday A Zone": False, 
-            "French Holiday B Zone": False,
-            "French Holiday C Zone": False
-        }
-
-        holidays_today["French Public Holiday"] = True if today_date in holidays.France() else False
-
-        # Get french public holidays per zone
-        holidays_dict = requests.get(self.FRENCH_VACATION_URL, headers=self.HEADERS).json()
-        holidays_dict = [holidays_item["fields"] for holidays_item in holidays_dict if "Zone" in holidays_item["fields"]["zones"]]
-        for holidays_period in holidays_dict:
-            if date.fromisoformat(holidays_period["start_date"]) <= today_date < date.fromisoformat(holidays_period["end_date"]):
-                for zone in ("A", "B", "C"):
-                    if zone in holidays_period["zones"]:
-                        holidays_today[f"French Holiday {zone} Zone"] = True
-                    
-        return holidays_today
 
     def _check_attractions(self):
         attraction_response = requests.get(self.ATTRACTION_URL, headers=self.HEADERS).json()["result"]["attractions"]
@@ -81,10 +46,7 @@ class WaitingTimeGetter:
 
         # locale.setlocale(category=locale.LC_ALL, locale="French")
 
-        waiting_time_dict["Datetime"] = datetime.now()
-        waiting_time_dict["Weather"] = self.weather.get_detailed_status()
-        waiting_time_dict["Temperature"] = self.weather.get_temperature("celsius")["temp"]   
-        waiting_time_dict["Holidays"] = self._get_today_holidays()
+        waiting_time_dict["Datetime"] = datetime.datetime.now()
 
         return waiting_time_dict
 
