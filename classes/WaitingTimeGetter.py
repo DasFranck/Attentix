d8458a426c6807b9970831943864c1fa1665550c
DasFranck Hochstaetter <dasfranck@gmx.com>
2019-04-01 12:30:03 +0200

    Change get_waiting_time() output (classes/WaitingTimeGetter.py)

--- classes/WaitingTimeGetter.py
+++ classes/WaitingTimeGetter.py
@@ -13,16 +13,11 @@ class WaitingTimeGetter:
     ATTENTIX_URL = "https://www.parcasterix.fr/webservices/api/attentix.json?device=android&version=320&apiversion=1&lang=fr"
     HEADERS = { "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 6.0.1; Redmi 3S MIUI/8.9.6)" }
 
-    attractions = {}
+    attraction_list = {}
 
     def __init__(self):
-        self._get_attractions()
+        self._get_attraction_list()
         self._check_attractions()
-
-    def _get_attractions(self):
-        with open("attractions.json") as attractions_file:
-            attractions = json.load(attractions_file)
-            self.attractions = OrderedDict(sorted(attractions.items(), key=lambda t: t[0]))
 
     def _check_attractions(self):
         attraction_response = requests.get(self.ATTRACTION_URL, headers=self.HEADERS).json()["result"]["attractions"]
@@ -31,19 +26,25 @@ class WaitingTimeGetter:
                 if self.attractions[item["code"]].lower() != item["title"].lower():
                     logging.warning(f"Attraction name doesn't match {item['code']}\nPlease check if attractions have been modified.")
 
+    def _get_attraction_list(self):
+        with open("attractions.json") as attractions_file:
+            attractions = json.load(attractions_file)
+            self.attractions = OrderedDict(sorted(attractions.items(), key=lambda t: t[0]))
+
+    def get_attraction_list(self):
+        return self.attraction_list
+
     def get_waiting_time(self):
-        waiting_time_dict = OrderedDict([("WaitingTime", {})])
+        waiting_time_dict = OrderedDict()
         api_response = requests.get(self.ATTENTIX_URL, headers=self.HEADERS).json()["latency"]["latency"]
 
         for item in self.attractions.items():
             attraction = next(waiting_time for waiting_time in api_response if waiting_time["attractionid"] == item[0])
             try:
-                waiting_time_dict["WaitingTime"][item[1]] = int(attraction["latency"])
+                waiting_time_dict[item[1]] = int(attraction["latency"])
             except (ValueError, KeyError):
                 logging.debug(f"Can't translate \"{attraction['latency'] if 'latency' in attraction else 'NONE'}\"")
-                waiting_time_dict["WaitingTime"][item[1]] = None
-
-        waiting_time_dict["Datetime"] = datetime.datetime.now()
+                waiting_time_dict[item[1]] = None
 
         return waiting_time_dict
 
